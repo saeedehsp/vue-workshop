d58751c16d0a88b60d94a31a77ba1f10d04c12ed
saeedehsp <s.sadeghpour97@gmail.com>
2019-12-28 13:17:52 +0330

    add workshop src

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
+/* eslint-disable vue/require-v-for-key */
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+    <div class="section">
+      <div class="title"> reactivity and data-binding: </div>
+      <div >{{message}}</div>
+      <input type="text" v-model="message" placeholder="input">
+    </div>
+    <!-- event handling -->
+    <div class="section">
+      <div class="title"> event handling:</div>
+      <div>  counter: {{counter}}</div>
+      <button @click="counter++">plus</button>
+    </div>
+    <!-- conditional rendering -->
+    <div class="section">
+      <div class="title"> conditional rendering:</div>
+      <div v-if="type === 'b'">type is b</div>
+      <div v-else>not b</div>
+    </div>
+    <!-- list rendering -->
+    <div class="section">
+      <div class="title"> list rendering:</div>
+      <div>  items:</div>
+      <div v-for="item in items" :key="item.id">
+        {{item.name}}
+      </div>
+    </div>
+
+    <!-- computed properties -->
+    <div class="section">
+      <div class="title">computed properties:</div>
+      <div> price: {{price}} </div>
+      <div> tax: {{tax}}</div>
+    </div>
+    <!-- component example -->
+    <div class="section">
+      <div class="title"> component example:</div>
+      <dropdown @update:option="option" number="option"></dropdown>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import dropdown from './components/dropdown.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components:{
+    dropdown
+  },
+  data() {
+    return {
+      message:'',
+      counter: 0,
+      items:[{
+          id:0,
+          name: 'book'},
+        {
+          id:1,
+         name: 'video'
+        },
+        {
+          id:2,
+          name: 'game'
+      }],
+      type: 'a',
+      price : 200,
+      item: ''
+    }
+  },
+  methods: {
+    option(value){
+      // eslint-disable-next-line no-console
+      console.log(value)
+    }
+  },
+  computed: {
+    tax(){
+      return this.price * 0.1
+    }
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.section{
+  margin: 2% 0 2% 0
+}
+.title{
+  color: #16693b;
 }
 </style>
---
+++ src/components/dropdown.vue
@@ -0,0 +1,26 @@
+<template>
+  <select name="dropdown" id="dropdown" @change="selectUpdate" v-model="selectedOption">
+    <option value="option1"> {{number}} 1  </option>
+    <option value="option2"> {{number}} 2 </option>
+    <option value="option3"> {{number}} 3 </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'dropdown',
+  props:{
+    number: String
+  },
+  data() {
+    return {
+      selectedOption:''
+    }
+  },
+  methods: {
+    selectUpdate(){
+      this.$emit('update:option',this.selectedOption)
+    }
+  },
+}
+</script>
